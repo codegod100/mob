1e144015b607b84e697095a561e63fd69ec370b1
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-09-03 16:31:34 +0000

    updater plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -56,6 +56,7 @@ async fn verify(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             app.manage(Mutex::new(AppState {
                 keypair: KeyPair::new(),
