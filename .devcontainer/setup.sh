6cf261e2af258abb466db1edc23d4788bc44f3f5
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-09-03 04:06:20 +0000

    path changes

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -30,4 +30,3 @@ cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
-cargo install tauri-cli@2.0.0-rc.9
